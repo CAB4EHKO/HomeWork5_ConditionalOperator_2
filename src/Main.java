72cc2e661825448c699cf1d0443a297eb66b7efb
Savchenko <savchenko.ra@yandex.ru>
2023-02-15 12:45:06 +0300

    Solution of the first task.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+    }
+    public static void task1() {
+        System.out.println("Задача 1.");
+        int clientOS = 1;
+        String i = "Установите версию приложения для IOS по ссылке:";
+        String a = "Установите версию приложения для Android по ссылке:";
+        if (clientOS == 0) {
+            System.out.println(i);
+        }
+        if (clientOS == 1) {
+            System.out.println(a);
+        }
     }
 }
