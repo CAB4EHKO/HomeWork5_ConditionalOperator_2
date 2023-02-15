bfac00c4cbe1d40f3f1f42b4e1f8f20755b2cca4
Savchenko <savchenko.ra@yandex.ru>
2023-02-15 13:07:27 +0300

    Solution of the second task.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
+
     public static void task1() {
         System.out.println("Задача 1.");
         int clientOS = 1;
@@ -11,6 +13,25 @@ public class Main {
             System.out.println(i);
         }
         if (clientOS == 1) {
+            System.out.println(a);
+        }
+    }
+    public static void task2() {
+        System.out.println("Задача 2.");
+        int clientOS = 1;
+        int clientDeviceYear = 2014;
+        String i = "Установите версию приложения для IOS по ссылке:";
+        String a = "Установите версию приложения для Android по ссылке:";
+        String oldI = "Установите облегчунную версию приложения для IOS по ссылке:";
+        String oldA = "Установите облегчунную версию приложения для Android по ссылке:";
+        if (clientOS == 0 && clientDeviceYear < 2015) {
+            System.out.println(oldI);
+        } else if (clientOS == 0) {
+            System.out.println(i);
+        }
+        if (clientOS == 1 && clientDeviceYear < 2015) {
+            System.out.println(oldA);
+        } else if (clientOS == 1) {
             System.out.println(a);
         }
     }
